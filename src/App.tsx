293e45e224e7c3507e59be57c08f436d0cf6f764
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-09-26 22:39:54 -0300

    styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1 `
+  font-size: 36px;
+  color: #14ff84;
+`
 
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }

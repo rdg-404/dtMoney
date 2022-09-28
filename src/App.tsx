4f705af0396b8ef10af8a0053dd5424baff1ac62
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-09-27 23:04:06 -0300

    cria e estiliza o component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { Header } from "./components/header";
 import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header/>
       <GlobalStyle/>
-    </div>
+    </>
   );
 }
 

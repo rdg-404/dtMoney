7491a7ac9d687ccf13bd6f355205a38a7b4d475f
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-09-27 22:34:26 -0300

    importando as fontes

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -38,6 +38,18 @@ body{
     -webkit-font-smoothing: antialiased;
 }
 
+body, textarea, button, input{
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+} 
+
+h1, h2, h3, h4, h5, h6, strong {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 600;
+}
+
+
+
 button{
     cursor: pointer;
 }

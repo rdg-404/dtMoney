cd568d954d0bfef7a9f21540aab368e030a593e9
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-10-04 22:42:26 -0300

    add miragejs and get the data by fetch

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from 'miragejs'; 
+
+createServer({
+  routes(){
+    this.namespace = 'api'; //pega qualquer coisa que vinher acompanhado de api
+
+
+    ///route transactions retorna os dados
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction01',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createAt: new Date()
+        }
+      ]
+    })
+  }
+});
 
 
 ReactDOM.render(

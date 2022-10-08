3d5fd998925148ccbeec2e70a2f68a7156c4dbb9
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-10-07 23:16:56 -0300

    add modal lib and config the open and close modal

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,39 @@
+import { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
+import Modal from 'react-modal';
+
+
 import { Container, Content } from './style';
 
 
 export function Header(){
+
+    const [isNewTransactionModalOpen, setIsNewTransactionModal] = useState(false);
+
+
+
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModal(true)
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModal(false)
+    }
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={handleOpenNewTransactionModal}>
                     Nova transação
                 </button>
+
+
+                <Modal 
+                    isOpen={isNewTransactionModalOpen} ///mostra que o modal está aberto 
+                    onRequestClose={handleCloseNewTransactionModal} //fechar modal ao clicar fora da area no esc
+                >
+                    <h2>Cadastrar transação</h2>
+                </Modal>
             </Content>
         </Container>
     )

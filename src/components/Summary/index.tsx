8a08bdbd52ea232a7d23e43eeaf33a7928cba4cc
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-10-28 22:52:44 -0300

    deixa dinamico os valores do summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -13,6 +13,35 @@ export function Summary(){
     const {transactions} = useContext(TransactionContext);
 
 
+    // const totalDeposits = transactions.reduce((acc, transaction) =>{
+    //     if(transaction.type === 'deposito'){
+    //         return acc + transaction.amount;
+    //     }
+
+    //     return acc;
+    // }, 0);
+
+
+    //percorre os dados e verifica
+    const summary = transactions.reduce((acc, transaction) =>{
+
+        if(transaction.type === 'deposito'){
+            acc.deposito += transaction.amount;
+            acc.total += transaction.amount;
+        }
+        else if(transaction.type === 'saida'){
+            acc.saida += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+        return acc;
+    }, {
+        //zera os valores
+        deposito: 0,
+        saida: 0,
+        total: 0,
+
+    })
+
 
     return (
         <Container>
@@ -21,21 +50,37 @@ export function Summary(){
                     <p>Entradas</p>
                     <img src={entradasImg} alt="Entradas" />
                 </header>
-                <strong>R$ 1000,00</strong>
+                <strong>
+                    { new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                    }).format(summary.deposito)}    
+                </strong>
             </div>
             <div>
                 <header>
                     <p>Saidas</p>
                     <img src={saidasImg} alt="Saidas" />
                 </header>
-                <strong>- R$ 500,00</strong>
+                <strong>
+                    -
+                    { new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL', 
+                    }).format(summary.saida)} 
+                </strong>
             </div>
             <div className='highlight-color'>
                 <header>
                     <p>Total</p>
                     <img src={totalImg} alt="Total" />
                 </header>
-                <strong>R$ 500,00</strong>
+                <strong>
+                { new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                }).format(summary.total)} 
+                </strong>
             </div>
 
         </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -12,22 +12,22 @@ createServer({
   seeds(server) {
     server.db.loadData({
       transactions: [
-        {
-          id: 1,
-          title: 'Desenvolvedor Front End',
-          type: 'deposito',
-          category: 'Dev',
-          amount: 4000,
-          createdAt: new Date('2022-05-23 08:30:00'),
-        },
-        {
-          id: 2,
-          title: 'Aluguel',
-          type:'saida',
-          category: 'Casa',
-          amount: -1100,
-          createdAt: new Date('2023-05-25 14:00:00'),
-        },
+        // {
+        //   id: 1,
+        //   title: 'Desenvolvedor Front End',
+        //   type: 'deposito',
+        //   category: 'Dev',
+        //   amount: 4000,
+        //   createdAt: new Date('2022-05-23 08:30:00'),
+        // },
+        // {
+        //   id: 2,
+        //   title: 'Aluguel',
+        //   type:'saida',
+        //   category: 'Casa',
+        //   amount: -1100,
+        //   createdAt: new Date('2023-05-25 14:00:00'),
+        // },
       ]
     })
   },
